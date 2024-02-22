94a23929400b8c68b54d04960fd14a1ac42f3604
Justin Calleja <justin.calleja@playnorth.com>
2024-02-22 20:59:13 +0100

    vite proxies to nest

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,15 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   root: "client",
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        // changeOrigin: true,
+        // configure: (proxy, options) => {
+        //   // proxy will be an instance of 'http-proxy'
+        // },
+      },
+    },
+  },
 });
